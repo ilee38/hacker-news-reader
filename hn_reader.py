d4dbeb9c2b7015a762138a032c9cbf41a1f2259e
Iram Lee <iram.lee@gmail.com>
2023-02-06 12:10:03 -0600

    add elapsed time to the story

--- hn_reader.py
+++ hn_reader.py
@@ -2,8 +2,10 @@
 
 import json
 import requests
-from datetime import date
 import time
+from collections import namedtuple
+from datetime import timedelta
+
 
 # URI prefix for the Hacker News v0 API
 API_ENDPOINT_PREFIX = "https://hacker-news.firebaseio.com/v0"
@@ -11,10 +13,31 @@ API_ENDPOINT_PREFIX = "https://hacker-news.firebaseio.com/v0"
 def print_header():
    local_time = time.localtime()
 
-   print("\n**********************************************************")
-   print("            Hacker News - Top Stories \n")
-   print(f"            {time.asctime(local_time)}")
-   print("**********************************************************\n")
+   print("\n******************************************************************")
+   print("                  Hacker News - Top Stories \n")
+   print(f"                  {time.asctime(local_time)}")
+   print("******************************************************************\n")
+
+
+def get_elapsed_time(story_timestamp):
+   """
+      Returns the elapsed time from a given timestamp to the current time.
+   """
+   time_value = None
+   time_units = None
+
+   current_time = timedelta(seconds=time.time(),)
+   story_time = timedelta(seconds=story_timestamp)
+   time_delta = current_time - story_time
+
+   if time_delta.seconds // 3600 > 0:
+      time_value = int(time_delta.seconds // 3600)
+      time_units = "hours" if time_value > 1 else "hour"
+   else:
+      time_value = int(time_delta.seconds // 60)
+      time_units = "minutes" if time_value > 1 else "minute"
+
+   return time_value, time_units
 
 
 def get_story_properties(story_id):
@@ -42,11 +65,12 @@ def print_stories(stories_ids):
       title = story["title"]
       url = story["url"] if "url" in story else ""
       score = story["score"]
+      time_value, time_unit = get_elapsed_time(story["time"])
 
       print(f"[{i+1}]. {title}")
       print(f"{url}")
-      print(f"score: {score}")
-      print("----------")
+      print(f"{score} points | {time_value} {time_unit} ago")
+      print("-------------------------\n")
 
 
 def get_top_stories():
